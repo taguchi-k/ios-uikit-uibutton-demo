0b3cf1ec2f665a0b1250ba25e497f969d6b84f11
k-moto <k.motoyama@smartdt.jp>
2017-02-27 21:11:48 +0900

    add UIButton

---
+++ ios-uikit-uibutton-demo/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ios-uikit-uibutton-demo
+//
+//  Created by k_motoyama on 2017/02/27.
+//  Copyright © 2017年 k_moto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTitle()
+        setupTitleColor()
+        getCurrentTitle()
+        setupImage()
+    }
+    
+    // 現在表示されているタイトルを取得する
+    private func getCurrentTitle(){
+        print(button.currentTitle!)
+    }
+    
+    // ボタンのタイトルを設定する
+    private func setupTitle(){
+        button.setTitle("ボタン", for: .normal)
+    }
+    
+    // ボタンのタイトルの色を設定する
+    private func setupTitleColor(){
+        button.setTitleColor(UIColor.red, for: .normal)
+    }
+    
+    // ボタンのタイトルの影を設定する
+    private func setupTitleShadowColor(){
+        button.setTitleShadowColor(UIColor.gray, for: .normal)
+        button.titleLabel!.shadowOffset = CGSize(width: 3, height: 3)
+
+    }
+    
+    // ボタンに表示する画像を設定する
+    private func setupImage(){
+        let image = UIImage(named:"apple.png")
+        button.setImage(image, for: .normal)
+        button.sizeToFit()
+        button.center = self.view.center
+    }
+
+}
+
